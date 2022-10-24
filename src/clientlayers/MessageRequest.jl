127d14dc3df6912b3fa1017e542f52a361bbea1e
Nick Robinson <npr251@gmail.com>
2022-10-24 18:36:17 +0100

    Define our own simpler Version type

--- src/clientlayers/MessageRequest.jl
+++ src/clientlayers/MessageRequest.jl
@@ -2,6 +2,7 @@ module MessageRequest
 
 using URIs
 using ..Messages, ..Parsers
+using ..Strings: HTTPVersion
 
 export messagelayer
 
@@ -12,7 +13,7 @@ Construct a [`Request`](@ref) object from method, url, headers, and body.
 Hard-coded as the first layer in the request pipeline.
 """
 function messagelayer(handler)
-    return function(method::String, url::URI, headers::Headers, body; response_stream=nothing, http_version=v"1.1", kw...)
+    return function(method::String, url::URI, headers::Headers, body; response_stream=nothing, http_version=HTTPVersion(1,1), kw...)
         req = Request(method, resource(url), headers, body; url=url, version=http_version, responsebody=response_stream)
         return handler(req; response_stream=response_stream, kw...)
     end
