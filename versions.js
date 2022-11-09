59f2fb664fff82598e7f79fcf810f20bdb2b7204
Documenter.jl <documenter@juliadocs.github.io>
2022-11-09 07:16:04 +0000

    delete history

---
+++ versions.js
@@ -0,0 +1,13 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v1.5",
+  "v1.4",
+  "v1.3",
+  "v1.0",
+  "v0.9",
+  "v0.8",
+  "v0.6",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v1.5.2";
+var DOCUMENTER_STABLE = "stable";
